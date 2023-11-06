a36f835b54fca811c6b5acc96cc11f292c87ba9a
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-06 10:25:49 +0500

    df

--- if09.py
+++ if09.py
@@ -9,4 +9,9 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    x1=a%10
+    x2=a//10
+    n=x1*10+x2
+
+    return n<=a
+print(main(21))
